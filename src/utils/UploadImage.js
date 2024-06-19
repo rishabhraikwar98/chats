4503d635a96767562dce4318384f710faa9a8377
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2024-06-19 14:18:25 +0530

    minor fix

--- src/utils/UploadImage.js
+++ src/utils/UploadImage.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-const token = localStorage.getItem("access_token");
 // crop = fit or pad
 export const UploadImage = async (file, crop = "pad") => {
+  axios.defaults.headers.common["Content-Type"] = "multipart/form-data"
   const formData = new FormData();
   formData.append("file", file);
   try {
     const response = await axios.post(BASE_URL + "/api/v1/upload", formData, {
       params: {
         crop,
-      },
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: "Bearer " + token,
-      },
+      }
     });
     const image_url = response.data.image_url;
     return image_url;
